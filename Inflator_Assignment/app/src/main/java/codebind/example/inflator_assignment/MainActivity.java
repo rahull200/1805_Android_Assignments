cadc11e4f7ce2dd0fb20036fb6cf4df5a6faed15
rahulbandodkar <59685264+rahull04@users.noreply.github.com>
2020-09-17 22:51:32 +0530

    final

--- Inflator_Assignment/app/src/main/java/codebind/example/inflator_assignment/MainActivity.java
+++ Inflator_Assignment/app/src/main/java/codebind/example/inflator_assignment/MainActivity.java
@@ -2,15 +2,21 @@ package codebind.example.inflator_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.PrecomputedText;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     LinearLayout views;
     LayoutInflater l;
+    ScrollView scrollView;
     Button red,yellow,blue,green;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +27,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         yellow = (Button)findViewById(R.id.yellow);
         green = (Button)findViewById(R.id.green);
         blue = (Button)findViewById(R.id.blue);
+        scrollView = (ScrollView)findViewById(R.id.scroll);
 
         red.setOnClickListener(this);
         yellow.setOnClickListener(this);
         green.setOnClickListener(this);
         blue.setOnClickListener(this);
 
+        l = (LayoutInflater) getApplicationContext().getSystemService(getApplicationContext().LAYOUT_INFLATER_SERVICE);
         views = (LinearLayout)findViewById(R.id.views);
-        l=getLayoutInflater();
 
+        //Get height of the mobile screen
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int height = metrics.heightPixels;
 
-
+        //Set height of scrollview as height(phone) - 330 for the buttons to be seen
+        ViewGroup.LayoutParams params = scrollView.getLayoutParams();
+        params.height = height-330;
+        scrollView.setLayoutParams(params);
     }
 
     @Override
