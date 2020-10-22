bc2094b9a603296e8725e8f92b0da6a1012a2c28
rahulbandodkar <59685264+rahull04@users.noreply.github.com>
2020-10-22 14:32:59 +0530

    webview final

--- Assign10_Webview/app/src/main/java/codebind/example/assign10_webview/MainActivity.java
+++ Assign10_Webview/app/src/main/java/codebind/example/assign10_webview/MainActivity.java
@@ -1,12 +1,16 @@
 package codebind.example.assign10_webview;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -15,14 +19,30 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
+    Toolbar tb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tb = (Toolbar)findViewById(R.id.mytb);
+
+        setSupportActionBar(tb);
+
         webView = (WebView)findViewById(R.id.mywebview);
     //    String cont = "<html><body>Hello</body></html>";
 
+        doTask();
+
+
+
+        //   webView.loadUrl("file:///android_asset/profile.html");
+
+      //  webView.loadData(cont,"text/html","UTF-8");
+
+    }
+
+    private void doTask() {
         if(isNetworkAvailable()){
             //Enable javascript
             webView.getSettings().setJavaScriptEnabled(true);
@@ -36,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
 
             webView.loadUrl("https://www.youtube.com/");
         }else{
-
             //Enable javascript
             webView.getSettings().setJavaScriptEnabled(true);
 
@@ -46,11 +65,6 @@ public class MainActivity extends AppCompatActivity {
             webView.loadUrl("file:///android_asset/profile.html");
 
         }
-
-        //   webView.loadUrl("file:///android_asset/profile.html");
-
-      //  webView.loadData(cont,"text/html","UTF-8");
-
     }
 
 
@@ -58,5 +72,20 @@ public class MainActivity extends AppCompatActivity {
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.reload){
+            doTask();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
